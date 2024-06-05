822cd3f600d0a986d395831ab5d74ebfd2c37e2b
ashishjangid2905 <ashish.jangid2905@outlook.com>
2024-06-05 10:43:04 +0530

    ready to Deploy and host

--- becrm/settings.py
+++ becrm/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.69',]
+ALLOWED_HOSTS = ['192.168.1.69', '192.168.1.240', '45.251.117.2']
 
 
 # Application definition
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -158,6 +159,8 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -174,7 +177,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
-MEDIA_URL = '/media/' # Public URL at the browser
+MEDIA_URL = 'media/' # Public URL at the browser
 
 
 
@@ -185,162 +188,162 @@ MEDIA_URL = '/media/' # Public URL at the browser
 # SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 
-# CkEditor Configuration
+# # CkEditor Configuration
 
-customColorPalette = [
-    {"color": "hsl(4, 90%, 58%)", "label": "Red"},
-    {"color": "hsl(340, 82%, 52%)", "label": "Pink"},
-    {"color": "hsl(291, 64%, 42%)", "label": "Purple"},
-    {"color": "hsl(262, 52%, 47%)", "label": "Deep Purple"},
-    {"color": "hsl(231, 48%, 48%)", "label": "Indigo"},
-    {"color": "hsl(207, 90%, 54%)", "label": "Blue"},
-]
+# customColorPalette = [
+#     {"color": "hsl(4, 90%, 58%)", "label": "Red"},
+#     {"color": "hsl(340, 82%, 52%)", "label": "Pink"},
+#     {"color": "hsl(291, 64%, 42%)", "label": "Purple"},
+#     {"color": "hsl(262, 52%, 47%)", "label": "Deep Purple"},
+#     {"color": "hsl(231, 48%, 48%)", "label": "Indigo"},
+#     {"color": "hsl(207, 90%, 54%)", "label": "Blue"},
+# ]
 
-CKEDITOR_5_CONFIGS = {
-    "default": {
-        "toolbar": [
-            "heading",
-            "|",
-            "bold",
-            "italic",
-            "link",
-            "bulletedList",
-            "numberedList",
-            "blockQuote",
-            "imageUpload"
-        ],
-    },
-    "comment": {
-        "language": {"ui": "en", "content": "en"},
-        "toolbar": [
-            "heading",
-            "|",
-            "bold",
-            "italic",
-            "link",
-            "bulletedList",
-            "numberedList",
-            "blockQuote",
-        ],
-    },
-    "extends": {
-        "language": "en",
-        "blockToolbar": [
-            "paragraph",
-            "heading1",
-            "heading2",
-            "heading3",
-            "|",
-            "bulletedList",
-            "numberedList",
-            "|",
-            "blockQuote",
-        ],
-        "toolbar": [
-            "heading",
-            "|",
-            "outdent",
-            "indent",
-            "|",
-            "bold",
-            "italic",
-            "link",
-            "underline",
-            "strikethrough",
-            "code",
-            "subscript",
-            "superscript",
-            "highlight",
-            "|",
-            "bulletedList",
-            "numberedList",
-            "todoList",
-            "|",
-            "blockQuote",
-            "insertImage",
-            "|",
-            "fontSize",
-            "fontFamily",
-            "fontColor",
-            "fontBackgroundColor",
-            "removeFormat",
-            "insertTable",
-        ],
-        "image": {
-            "toolbar": [
-                "imageTextAlternative",
-                "|",
-                "imageStyle:alignLeft",
-                "imageStyle:alignRight",
-                "imageStyle:alignCenter",
-                "imageStyle:side",
-                "|",
-                "toggleImageCaption",
-                "|"
-            ],
-            "styles": [
-                "full",
-                "side",
-                "alignLeft",
-                "alignRight",
-                "alignCenter",
-            ],
-        },
-        "table": {
-            "contentToolbar": [
-                "tableColumn",
-                "tableRow",
-                "mergeTableCells",
-                "tableProperties",
-                "tableCellProperties",
-            ],
-            "tableProperties": {
-                "borderColors": customColorPalette,
-                "backgroundColors": customColorPalette,
-            },
-            "tableCellProperties": {
-                "borderColors": customColorPalette,
-                "backgroundColors": customColorPalette,
-            },
-        },
-        "heading": {
-            "options": [
-                {
-                    "model": "paragraph",
-                    "title": "Paragraph",
-                    "class": "ck-heading_paragraph",
-                },
-                {
-                    "model": "heading1",
-                    "view": "h1",
-                    "title": "Heading 1",
-                    "class": "ck-heading_heading1",
-                },
-                {
-                    "model": "heading2",
-                    "view": "h2",
-                    "title": "Heading 2",
-                    "class": "ck-heading_heading2",
-                },
-                {
-                    "model": "heading3",
-                    "view": "h3",
-                    "title": "Heading 3",
-                    "class": "ck-heading_heading3",
-                },
-            ]
-        },
-        "list": {
-            "properties": {
-                "styles": True,
-                "startIndex": True,
-                "reversed": True,
-            }
-        },
-        "htmlSupport": {
-            "allow": [
-                {"name": "/.*/", "attributes": True, "classes": True, "styles": True}
-            ]
-        },
-    },
-}
+# CKEDITOR_5_CONFIGS = {
+#     "default": {
+#         "toolbar": [
+#             "heading",
+#             "|",
+#             "bold",
+#             "italic",
+#             "link",
+#             "bulletedList",
+#             "numberedList",
+#             "blockQuote",
+#             "imageUpload"
+#         ],
+#     },
+#     "comment": {
+#         "language": {"ui": "en", "content": "en"},
+#         "toolbar": [
+#             "heading",
+#             "|",
+#             "bold",
+#             "italic",
+#             "link",
+#             "bulletedList",
+#             "numberedList",
+#             "blockQuote",
+#         ],
+#     },
+#     "extends": {
+#         "language": "en",
+#         "blockToolbar": [
+#             "paragraph",
+#             "heading1",
+#             "heading2",
+#             "heading3",
+#             "|",
+#             "bulletedList",
+#             "numberedList",
+#             "|",
+#             "blockQuote",
+#         ],
+#         "toolbar": [
+#             "heading",
+#             "|",
+#             "outdent",
+#             "indent",
+#             "|",
+#             "bold",
+#             "italic",
+#             "link",
+#             "underline",
+#             "strikethrough",
+#             "code",
+#             "subscript",
+#             "superscript",
+#             "highlight",
+#             "|",
+#             "bulletedList",
+#             "numberedList",
+#             "todoList",
+#             "|",
+#             "blockQuote",
+#             "insertImage",
+#             "|",
+#             "fontSize",
+#             "fontFamily",
+#             "fontColor",
+#             "fontBackgroundColor",
+#             "removeFormat",
+#             "insertTable",
+#         ],
+#         "image": {
+#             "toolbar": [
+#                 "imageTextAlternative",
+#                 "|",
+#                 "imageStyle:alignLeft",
+#                 "imageStyle:alignRight",
+#                 "imageStyle:alignCenter",
+#                 "imageStyle:side",
+#                 "|",
+#                 "toggleImageCaption",
+#                 "|"
+#             ],
+#             "styles": [
+#                 "full",
+#                 "side",
+#                 "alignLeft",
+#                 "alignRight",
+#                 "alignCenter",
+#             ],
+#         },
+#         "table": {
+#             "contentToolbar": [
+#                 "tableColumn",
+#                 "tableRow",
+#                 "mergeTableCells",
+#                 "tableProperties",
+#                 "tableCellProperties",
+#             ],
+#             "tableProperties": {
+#                 "borderColors": customColorPalette,
+#                 "backgroundColors": customColorPalette,
+#             },
+#             "tableCellProperties": {
+#                 "borderColors": customColorPalette,
+#                 "backgroundColors": customColorPalette,
+#             },
+#         },
+#         "heading": {
+#             "options": [
+#                 {
+#                     "model": "paragraph",
+#                     "title": "Paragraph",
+#                     "class": "ck-heading_paragraph",
+#                 },
+#                 {
+#                     "model": "heading1",
+#                     "view": "h1",
+#                     "title": "Heading 1",
+#                     "class": "ck-heading_heading1",
+#                 },
+#                 {
+#                     "model": "heading2",
+#                     "view": "h2",
+#                     "title": "Heading 2",
+#                     "class": "ck-heading_heading2",
+#                 },
+#                 {
+#                     "model": "heading3",
+#                     "view": "h3",
+#                     "title": "Heading 3",
+#                     "class": "ck-heading_heading3",
+#                 },
+#             ]
+#         },
+#         "list": {
+#             "properties": {
+#                 "styles": True,
+#                 "startIndex": True,
+#                 "reversed": True,
+#             }
+#         },
+#         "htmlSupport": {
+#             "allow": [
+#                 {"name": "/.*/", "attributes": True, "classes": True, "styles": True}
+#             ]
+#         },
+#     },
+# }
