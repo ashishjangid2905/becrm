dd52f6b57298c1788f9fa806db930e8273ad613f
ashishjangid2905 <ashish.jangid2905@outlook.com>
2024-06-07 17:03:33 +0530

    loaderimplimented

--- staticfiles/becrm/js/script.js
+++ staticfiles/becrm/js/script.js
@@ -82,6 +82,12 @@ $(document).ready(function () {
     }
   });
 
+  // loader implimentation
+
+  $("form").on("submit", function(){
+    $("#loader").show();
+  });
+
   // Fetch the sample data
 
   $.ajax({
