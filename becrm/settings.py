bcc0286eedae7b2993045e6f77f7f58f6ce31026
ashishjangid2905 <ashish.jangid2905@gmail.com>
2025-01-02 11:15:43 +0530

    host added

--- becrm/settings.py
+++ becrm/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.3.98', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.3.98', '127.0.0.1', '192.168.0.3', '122.176.98.137']
 
 
 # Application definition
